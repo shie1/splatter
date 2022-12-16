7dcede6defaa758455391d4f748e405523f62f9b
Shie1 <sonkolyw@gmail.com>
2022-12-16 23:56:52 +0100

    this shit fucking works again

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys, getopt, os, mimetypes, requests
+import sys
+import getopt
+import os
+import mimetypes
+import requests
 import spotify_spammer as spotify
+
 
 def nonblank_lines(f):
     for l in f:
@@ -7,18 +12,21 @@ def nonblank_lines(f):
         if line:
             yield line
 
+
 def main():
-    try: requests.get("https://google.com")
+    try:
+        requests.get("https://google.com")
     except requests.exceptions.ConnectionError:
         print("No internet access!")
         sys.exit(2)
-    
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "t:l:", ["threads=","audio","headless","login=","tracking"])
+        opts, args = getopt.getopt(sys.argv[1:], "t:l:", [
+                                   "threads=", "audio", "headless", "login=", "tracking"])
     except getopt.GetoptError as err:
         print(str(err).capitalize() + "!")
         sys.exit(2)
-    
+
     # Default Options
     options = {
         "threads": 1,
@@ -27,7 +35,7 @@ def main():
         "login": False,
         "tracking": False
     }
-    
+
     for o, a in opts:
         if o in ("-t", "--threads"):
             if(a.isnumeric() == False):
@@ -40,9 +48,9 @@ def main():
             options["headless"] = True
         elif o in ("-l", "--login"):
             options["login"] = True
-            
+
             userlist = a
-            
+
             if(os.path.exists(userlist) == False):
                 print("Userlist path invalid!")
                 sys.exit(2)
@@ -57,30 +65,37 @@ def main():
             print("NOTE: Tracking is still in development, currently it does nothing!\n")
         else:
             assert False, "unhandled option"
-    
+
     if(len(args) < 1):
         print("Please provide a Spotify playlist URL!")
         sys.exit(2)
-    
+
     url = args[0]
-    
+
     try:
-        try: req = requests.get(url).text
-        except requests.exceptions.MissingSchema: req = requests.get("https://" + url).text
-        except requests.exceptions.ConnectionError: req = ""
-    except requests.exceptions.ConnectionError: req = ""
-    
-    if(("spotify" in req) == False) or (("playlist" in req) == False):
+        try:
+            req = requests.get(url).text
+        except requests.exceptions.MissingSchema:
+            req = requests.get("https://" + url).text
+        except requests.exceptions.ConnectionError:
+            req = ""
+    except requests.exceptions.ConnectionError:
+        req = ""
+
+    if(("spotify" in req) == False):
         print("Please enter a valid playlist URL!")
         sys.exit(2)
-    
+
     try:
         if(len(options["userlist"]) < options["threads"]):
-            print(f"Can't start {options['threads']} threads with {len(userlist)} user(s)!")
+            print(
+                f"Can't start {options['threads']} threads with {len(userlist)} user(s)!")
             sys.exit(2)
-    except KeyError: ""
+    except KeyError:
+        ""
 
-    spotify.start(options,url)
+    spotify.start(options, url)
+
 
 if __name__ == "__main__":
     main()
--- spotify_spammer.py
+++ spotify_spammer.py
@@ -5,98 +5,112 @@ from threading import Thread
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from selenium import webdriver
 from random import uniform
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-def check_exists(css, driver):
+
+def check_exists(by, select, driver):
     try:
-        driver.find_element_by_css_selector(css)
+        driver.find_element(by, select)
     except NoSuchElementException:
         return False
     return True
 
-def check_exists_xpath(xpath, driver):
-    try:
-        driver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
 
 def smooth_type(text, element):
     text = text.split()
     for letter in text:
         element.send_keys(letter)
-        sleep(uniform(0.5,1))
+        sleep(uniform(0.5, 1))
     return
 
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
 
 def quickplay(user, password, playlist, driver, options):
     driver.delete_all_cookies()
     driver.get("https://www.google.com/search?q=spotify")
     sleep(2)
-    driver.get("https://accounts.spotify.com/en/login?continue=" + parse.quote(playlist.encode("utf-8")))
-    
+    driver.get("https://accounts.spotify.com/en/login?continue=" +
+               parse.quote(playlist.encode("utf-8")))
+
     if (user != None) and (password != None):
-        user_elem = driver.find_element_by_id("login-username")
-        password_elem = driver.find_element_by_id("login-password")
-        button_elem = driver.find_element_by_class_name("btn-green")
+        user_elem = driver.find_element(By.ID, "login-username")
+        password_elem = driver.find_element(By.ID, "login-password")
+        button_elem = driver.find_element(By.ID, "login-button")
 
         user_elem.clear()
-        sleep(uniform(0.1,0.25))
+        sleep(uniform(0.1, 0.25))
         password_elem.clear()
-        sleep(uniform(0.1,0.25))
+        sleep(uniform(0.1, 0.25))
 
         smooth_type(user, user_elem)
         smooth_type(password, password_elem)
-        sleep(uniform(0.5,1))
+        sleep(uniform(0.5, 1))
         button_elem.click()
-    
-    while True:
-        if(check_exists("#onetrust-accept-btn-handler", driver) == True):
-            try:
-                driver.find_element_by_css_selector("#onetrust-accept-btn-handler").click() #Accept cookies
-                sleep(5)
-            except: ""
 
-        #Shuffle
-        if(check_exists("button._39234eb5c173f8b6de80ed73820b1be8-scss[aria-checked=\"false\"]", driver) == True):
+    while True:
+        if(check_exists(By.CSS_SELECTOR, "#onetrust-accept-btn-handler", driver) == True):
             try:
-                driver.find_element_by_css_selector("button._39234eb5c173f8b6de80ed73820b1be8-scss").click()
-            except: ""
-                
-        #Repeat
-        if(check_exists("button.ebfd411a126f1e7bea6133f21b4ef88e-scss[aria-checked=\"false\"]", driver) == True) or (check_exists("button.ebfd411a126f1e7bea6133f21b4ef88e-scss[aria-checked=\"mixed\"]", driver) == True):
+                driver.find_element(
+                    By.CSS_SELECTOR, "#onetrust-accept-btn-handler").click()  # Accept cookies
+                sleep(5)
+            except:
+                ""
+
+        # Shuffle
+        if(check_exists(By.CSS_SELECTOR, "button[aria-label=\"Enable shuffle\"][aria-checked=\"false\"]", driver) == True):
             try:
-                driver.find_element_by_css_selector("button.ebfd411a126f1e7bea6133f21b4ef88e-scss").click()
-            except: ""
-            
-        #Play
-        if(check_exists_xpath('(.//button[contains(concat(" ",normalize-space(@class)," ")," _8e7d398e09c25b24232d92aac8a15a81-scss ")][@aria-label="Play"])[2]', driver) == True):
+                driver.find_element(By.CSS_SELECTOR,
+                                    "button[aria-label=\"Enable shuffle\"]").click()
+            except:
+                ""
+
+        # Repeat
+        if(check_exists(By.CSS_SELECTOR, "button[aria-label=\"Enable repeat\"][aria-checked=\"false\"]", driver) == True) or (check_exists(By.CSS_SELECTOR, "button.ebfd411a126f1e7bea6133f21b4ef88e-scss[aria-checked=\"mixed\"]", driver) == True):
             try:
-                driver.find_element_by_xpath('(.//button[contains(concat(" ",normalize-space(@class)," ")," _8e7d398e09c25b24232d92aac8a15a81-scss ")][@aria-label="Play"])[2]').click()
-            except ElementNotInteractableException: driver.send_keys(" ")
-            except NoSuchElementException: ""
-        
-        #Skip
+                driver.find_element(By.CSS_SELECTOR,
+                                    "button[aria-label=\"Enable repeat\"]").click()
+            except:
+                ""
+
+        # Play
+        if(check_exists(By.CSS_SELECTOR, 'button[data-encore-id="buttonPrimary"][data-testid="play-button"][aria-label="Play"][class="Button-sc-qlcn5g-0 futnNt"]', driver) == True):
+            try:
+                driver.find_element(By.CSS_SELECTOR,
+                                    'button[data-encore-id="buttonPrimary"][data-testid="play-button"][aria-label="Play"][class="Button-sc-qlcn5g-0 futnNt"]').click()
+            except ElementNotInteractableException:
+                driver.find_element(By.TAG_NAME, "body").send_keys(" ")
+            except NoSuchElementException:
+                ""
+
+        # Skip
         try:
-            time = driver.find_element_by_css_selector("div.playback-bar__progress-time").text
+            time = driver.find_element(By.CSS_SELECTOR,
+                                       "div.playback-bar__progress-time-elapsed").text
+            print(time)
             mins, secs = time.split(":")
             mins = int(mins)
             secs = int(secs)
+            print(mins, secs)
             if((mins != 0) == True) or ((secs > 35) == True):
-                driver.find_element_by_css_selector("button[aria-label=\"Next\"]").click()
-        except NoSuchElementException: ""
-        
+                driver.find_element(By.TAG_NAME,
+                                    "body").send_keys(Keys.ALT, Keys.ARROW_RIGHT)
+        except NoSuchElementException:
+            ""
+
         sleep(5)
+
 
 def browser(options):
     chrome_options = webdriver.ChromeOptions()
-    if(options["audio"] == False): chrome_options.add_argument("--mute-audio")
-    if(options["headless"] == True): chrome_options.add_argument("--headless")
-    return webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),chrome_options=chrome_options)
+    if(options["audio"] == False):
+        chrome_options.add_argument("--mute-audio")
+    if(options["headless"] == True):
+        chrome_options.add_argument("--headless")
+    return webdriver.Chrome("chromedriver.exe", chrome_options=chrome_options)
 
-def start(options,playlist):
-    for x in range(0,int(options["threads"])):
+
+def start(options, playlist):
+    for x in range(0, int(options["threads"])):
         if(options["login"] == True):
             u = options["userlist"][x].split(":")
             if(len(u) != 2):
@@ -105,4 +119,5 @@ def start(options,playlist):
         else:
             user, password = None, None
 
-        Thread(target=quickplay,args=(user,password,playlist,browser(options),options)).start()
+        Thread(target=quickplay, args=(user, password,
+               playlist, browser(options), options)).start()
