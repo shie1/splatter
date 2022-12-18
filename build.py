afd47ea3a4ce846782f493a11ecdff0a6308a761
Shie1 <sonkolyw@gmail.com>
2022-12-18 16:17:38 +0100

    added build.py

---
+++ build.py
@@ -0,0 +1,55 @@
+from platform import system as os
+from os import system as exec
+from glob import glob
+from os import chdir, path, getcwd, mkdir, remove
+import platform
+from shutil import rmtree
+
+def data(file, alias=""):
+    if(alias == ""): alias = file
+    if(os() == "Windows"):
+        d = ";"
+    else:
+        d = ":"
+    return f'--add-data "{file}{d}{file}"'
+
+def hiddenimport(module):
+    return f'--hidden-import {module}'
+
+name = path.split(getcwd())[-1]
+imports = []
+
+try:
+    for module in open("requirements.txt", "r").read().split("\n"):
+        imports.append(module.split("==")[0])
+except: ""
+
+install = f'pyinstaller -y --distpath "dist" --name "{name}"'
+
+for file in glob("*.py"):
+    if(file == ""):
+        continue
+    install += f" {data(file)}"
+    imports.append(file)
+
+for module in imports:
+    if(module == ""):
+        continue
+    install += f" {hiddenimport(module)}"
+
+if(path.exists("main.py") == False):
+    src = glob("*.py")[0]
+else:
+    src = "main.py"
+
+install += f" -F {src}"
+
+print(install)
+exec(install)
+
+try: remove(name + '.spec')
+except: ""
+try: rmtree('__pycache__')
+except: ""
+try: rmtree('build')
+except: ""
